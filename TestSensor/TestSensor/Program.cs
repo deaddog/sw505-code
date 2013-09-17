7feda4c3fca9906018450ad3eae61851b1e48403
Bruno Thalmann <mr.thalmann@gmail.com>
2013-09-17 12:06:09 +0200

    Added testproject for test of the ultrasonic and maybe other tests later. Also added latest addition to sensor.tex

---
+++ TestSensor/TestSensor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NKH.MindSqualls;
+
+namespace TestSensor
+{
+    class Program
+    {
+        
+        //Press any key for result, enter for return
+
+        static void Main(string[] args)
+        {
+            NxtBrick b = new NxtBrick(NxtCommLinkType.Bluetooth, 10);
+            NxtUltrasonicSensor u = new NxtUltrasonicSensor();
+
+            b.Sensor1 = u;
+
+            u.PollInterval = 20;
+
+            b.Connect();
+
+            while (true)
+            {
+                if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine(u.DistanceCm.ToString());
+                }
+            }
+
+        }
+    }
+}
