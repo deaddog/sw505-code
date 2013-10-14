4dda84c610741e4cc83e270e2e473aacfbb4c739
Bruno Thalmann <mr.thalmann@gmail.com>
2013-10-14 15:02:18 +0200

    changed namespace name from backgroundsubstraction to backgroundsubtraction

--- BackgroundSubstraction/BackgroundSubstraction/Properties/Resources.Designer.cs
+++ BackgroundSubstraction/BackgroundSubstraction/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BackgroundSubstraction.Properties {
+namespace BackgroundSubtraction.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BackgroundSubstraction.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BackgroundSubstraction.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BackgroundSubtraction.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
