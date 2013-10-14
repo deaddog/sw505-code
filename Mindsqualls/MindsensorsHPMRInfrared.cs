58878556bb54365673f032692ba0b93161b36ea9
deaddog <mikkelslsl@gmail.com>
2013-10-14 13:41:45 +0200

    Started work on class for the Mindsensors High Precision Medium Range Infrared Distance Sensor - "MindsensorsHPMRInfrared".

---
+++ Mindsqualls/MindsensorsHPMRInfrared.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// Implemented using the design priciples og HiTechnicCompassSensor and NxtUltrasonicSensor
+
+namespace NKH.MindSqualls
+{
+    public class MindsensorsHPMRInfrared : NxtDigitalSensor
+    {
+        public MindsensorsHPMRInfrared()
+            : base()
+        {
+        }
+
+        internal override void InitSensor()
+        {
+            base.InitSensor();
+
+            //Set sensor module mode to GP2Y0A21YK (Medium Range)
+            CommandToAddress(0x41, 0x33);
+
+            //Energize sensor module
+            CommandToAddress(0x41, 0x45);
+        }
+    }
+}
