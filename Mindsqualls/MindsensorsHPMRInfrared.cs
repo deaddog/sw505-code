0754a24bb02980013bb60f57cd739a67789e1d1b
deaddog <mikkelslsl@gmail.com>
2013-10-14 13:48:40 +0200

    Added commands that reads the distance from the sensor to an object.

--- Mindsqualls/MindsensorsHPMRInfrared.cs
+++ Mindsqualls/MindsensorsHPMRInfrared.cs
@@ -21,5 +21,30 @@ namespace NKH.MindSqualls
             //Energize sensor module
             CommandToAddress(0x41, 0x45);
         }
+
+        #region I2C protocol
+
+        public byte? DistanceMSB()
+        {
+            return ReadByteFromAddress(0x45);
+        }
+        public byte? DistanceLSB()
+        {
+            return ReadByteFromAddress(0x44);
+        }
+
+        public UInt16? Distance
+        {
+            get
+            {
+                byte? msb = DistanceMSB(), lsb = DistanceLSB();
+                if (msb.HasValue && lsb.HasValue)
+                    return (ushort?)((msb.Value << 8) & lsb.Value);
+                else
+                    return (ushort?)null;
+            }
+        }
+
+        #endregion
     }
 }
