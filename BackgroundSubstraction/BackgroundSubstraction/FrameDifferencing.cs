20ab0fc7c598beb28335794e1efc0627bdc7f69e
Bruno Thalmann <mr.thalmann@gmail.com>
2013-10-14 14:52:25 +0200

    Added function to get middle coordinates from contour

--- BackgroundSubstraction/BackgroundSubstraction/FrameDifferencing.cs
+++ BackgroundSubstraction/BackgroundSubstraction/FrameDifferencing.cs
@@ -10,12 +10,12 @@ using System.Drawing;
 
 namespace BackgroundSubstraction
 {
+    //Uses Frame Differencing for finding objects between two Bitmaps
     public class FrameDifferencing
     {
-        public FrameDifferencing()
-        {
-        }
+        public FrameDifferencing() { }
 
+        //Finds the absolute difference and thresholdes it
         public Bitmap Diff(Bitmap current, Bitmap previous, int threshold = 50)
         {
             Image<Bgr, Byte> frame = new Image<Bgr, byte>(current);
@@ -28,7 +28,9 @@ namespace BackgroundSubstraction
             return diff.ToBitmap();
         }
 
-        public Bitmap FindContour(Bitmap thresholdedBitmap, Bitmap targetBitmap, int countourThreshold = 500)
+        //Finds the contour from the thresholded bitmap and draws it on to the targetBitmap with a cross in the middle
+        //contourThreshold is the size of the contour you want to find
+        public Bitmap FindContour(Bitmap thresholdedBitmap, Bitmap targetBitmap, int contourThreshold = 500)
         {
             Image<Bgr, Byte> difference = new Image<Bgr, byte>(thresholdedBitmap);
             Image<Bgr, Byte> target = new Image<Bgr, byte>(targetBitmap);
@@ -46,7 +48,7 @@ namespace BackgroundSubstraction
                     Contour<Point> currentContour = contours.ApproxPoly(contours.Perimeter * 0.05, storage);
 
                     //Draw the detected contour on the image
-                    if (currentContour.Area > countourThreshold) //only consider contours with area greater than 100 as default then take from form control
+                    if (currentContour.Area > contourThreshold) //only consider contours with area greater than 100 as default then take from form control
                     {
                         target.Draw(currentContour.BoundingRectangle, new Bgr(Color.Red), 2);
 
@@ -57,6 +59,35 @@ namespace BackgroundSubstraction
                     }
                 }
             return target.ToBitmap();
+        }
+
+        public Point FindContourMiddleCoordinates(Bitmap thresholdedBitmap, Bitmap targetBitmap, int contourThreshold = 500)
+        {
+            Image<Bgr, Byte> difference = new Image<Bgr, byte>(thresholdedBitmap);
+            Image<Bgr, Byte> target = new Image<Bgr, byte>(targetBitmap);
+            int x = 0, y = 0;
+
+            using (MemStorage storage = new MemStorage()) //allocate storage for contour approximation
+                //detect the contours and loop through each of them
+                for (Contour<Point> contours = difference.Convert<Gray, Byte>().FindContours(
+                      Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE,
+                      Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_LIST,
+                      storage);
+                   contours != null;
+                   contours = contours.HNext)
+                {
+                    //Create a contour for the current variable to work with
+                    Contour<Point> currentContour = contours.ApproxPoly(contours.Perimeter * 0.05, storage);
+
+                    //Draw the detected contour on the image
+                    if (currentContour.Area > contourThreshold) //only consider contours with area greater than 100 as default then take from form control
+                    {
+                        //x and y coordinates for the middle of the contour
+                        x = currentContour.BoundingRectangle.X + (currentContour.BoundingRectangle.Width / 2);
+                        y = currentContour.BoundingRectangle.Y + (currentContour.BoundingRectangle.Height / 2);
+                    }
+                }
+            return new Point(x, y);
         }
     }
 }
