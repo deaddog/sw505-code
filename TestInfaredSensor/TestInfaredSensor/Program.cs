d9406aead3c807b20b612d6d4de351c46e1d3f80
stufkan <stufkan@gmail.com>
2013-10-16 11:48:06 +0200

    Modified the TestInfrared solution and performed test with it

--- TestInfaredSensor/TestInfaredSensor/Program.cs
+++ TestInfaredSensor/TestInfaredSensor/Program.cs
@@ -11,57 +11,55 @@ namespace TestInfaredSensor
 {
     class Program
     {
+        static MindsensorsHPMRInfrared u;
+        static StringBuilder sb;
+
         static void Main(string[] args)
         {
-            FileStream fs = new FileStream("results.txt", FileMode.Create);
-            var w = new StreamWriter(fs);
-            w.AutoFlush = true;
-            //Console.SetOut(w);
-            //Console.SetError(w);
+            sb = new StringBuilder();
+
 
             NxtBrick b = new NxtBrick(NxtCommLinkType.Bluetooth, 10);
-            NxtUltrasonicSensor u = new NxtUltrasonicSensor();
-
-
-            //NxtBluetoothConnection bc = new NxtBluetoothConnection("COM10");
-            //bc.Connect();
+            u = new MindsensorsHPMRInfrared();
 
             b.Sensor1 = u;
-            //u.PollInterval = 20;
 
             b.Connect();
 
+            //u.PollInterval = 500;
+            u.OnPolled += u_OnPolled;
+
             while (true)
             {
-                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
-                {
-                    b.Disconnect();
-                    //bc.Disconnect();
-                        return;
-                }
-                else
-                {
+                Console.WriteLine("distance:");
+                sb.Append(Console.ReadLine() + " ");
+
+                ConsoleKey k = Console.ReadKey(true).Key;
+
+                if (k == ConsoleKey.Enter)
                     u.Poll();
-                    
-                    byte? x = u.ReadMeasurementByteX(0);
-                    byte? y = u.ReadMeasurementByteX(1);
-                    if (x != null && y != null)
-                    {
-                        byte[] a = new byte[] { x.Value, y.Value };
-
-                        Console.WriteLine((y << 8)+x);
-                        //Console.WriteLine(BitConverter.ToInt16(a, 0));
-                    }
-                    
-                    for (int i = 0; i < 8; i++)
-                    {
-                        Console.WriteLine(i + ": " + u.ReadMeasurementByteX((byte)i));
-                    }
-                    
-                    
+                else if (k == ConsoleKey.Escape)
+                {
+                    writeFile();
+                    break;
                 }
-            }
 
+            }
+        }
+
+        static void writeFile()
+        {
+            using (StreamWriter outfile = new StreamWriter("results"))
+            {
+                outfile.Write(sb.ToString());
+            }
+        }
+
+        static void u_OnPolled(NxtPollable polledItem)
+        {
+            Console.WriteLine(u.Distance);
+            sb.Append(u.Distance);
+            sb.AppendLine();
         }
     }
 }
