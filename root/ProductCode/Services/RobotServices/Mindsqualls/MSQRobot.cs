2dd0548f69739298d398fa9d2a4217d193cfd891
cyborbob <rdf_errorzone@hotmail.com>
2013-11-04 09:28:18 +0100

    Added constructor chain to MSQRobot

--- root/ProductCode/Services/RobotServices/Mindsqualls/MSQRobot.cs
+++ root/ProductCode/Services/RobotServices/Mindsqualls/MSQRobot.cs
@@ -11,13 +11,30 @@ namespace Services.RobotServices.Mindsqualls
     public class MSQRobot : IRobot
     {
         private const byte SERIAL_PORT_NUMBER = 10;
-        private NxtBrick brick = new NxtBrick(NxtCommLinkType.Bluetooth, SERIAL_PORT_NUMBER);
+        private const int SENSOR_POLL_INTERVAL = 20;
+        private NxtBrick robot;  
 
-        public MSQRobot()
+        #region cTor Chain
+
+        public MSQRobot() : this(SERIAL_PORT_NUMBER, SENSOR_POLL_INTERVAL) { }
+
+        public MSQRobot(byte serialPort, int sensorPollInteval)
         {
+            robot = new NxtBrick(NxtCommLinkType.Bluetooth, serialPort);
 
+            NxtUltrasonicSensor sensor1 = new NxtUltrasonicSensor();
+            NxtUltrasonicSensor sensor2 = new NxtUltrasonicSensor();
+
+            sensor1.PollInterval = sensorPollInteval;
+            sensor2.PollInterval = sensorPollInteval;
+
+            robot.Sensor1 = sensor1;
+            robot.Sensor2 = sensor2;
+
+            robot.Connect();
         }
         
+        #endregion
 
         public void TurnRobot(uint degrees)
         {
