c9096c0264a82573f0397f438704cef5b19fa81a
deaddog <mikkelslsl@gmail.com>
2013-11-05 10:36:33 +0100

    Implemented GetHue method.

--- ObjectFromColor/TrackColorForm/ColorTracking.cs
+++ ObjectFromColor/TrackColorForm/ColorTracking.cs
@@ -14,12 +14,29 @@ namespace TrackColorForm
         {
         }
 
+        private static unsafe float GetHue(byte* p)
+        {
+            byte min = Math.Min(p[0], Math.Min(p[1], p[2]));
+            byte max = Math.Max(p[0], Math.Max(p[1], p[2]));
+
+            if (max == min)
+                return 0;
+            else if (max == p[2])
+                return (60f * (p[1] - p[0]) / (max - min) + 360f) % 360f;
+            else if (max == p[1])
+                return (60f * (p[0] - p[2]) / (max - min) + 120f + 360f) % 360f;
+            else if (max == p[0])
+                return (60f * (p[2] - p[1]) / (max - min) + 240f + 360f) % 360f;
+
+            throw new Exception("Calculation error in GetHue.");
+        }
+
         public static Image TrackColor(Bitmap src, double hueMin, double hueMax)
         {
             Bitmap output = new Bitmap(src.Width, src.Height);
-            BitmapData bdSrc = src.LockBits(new Rectangle(0,0,src.Width, src.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            BitmapData bdSrc = src.LockBits(new Rectangle(0, 0, src.Width, src.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
             BitmapData bdOutput = output.LockBits(new Rectangle(0, 0, src.Width, src.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-            
+
             int PixelSize = 3;
             unsafe
             {
@@ -31,18 +48,14 @@ namespace TrackColorForm
 
                     for (int j = 0; j < bdSrc.Width; j++)
                     {
-                        Color c = Color.FromArgb(row[j*PixelSize+2],row[j*PixelSize+1], row[j*PixelSize]);
-                        
-                        double hue = c.GetHue();
+                        float hue = GetHue(row + j * PixelSize);
 
-                        c = hue > hueMax || hue < hueMin ? Color.Black : Color.White;
+                        byte c = hue > hueMax || hue < hueMin ? (byte)0 : (byte)255;
 
-                        rowOutput[j * PixelSize + 2] = c.R;
-                        rowOutput[j * PixelSize + 1] = c.G;
-                        rowOutput[j * PixelSize] = c.B;
-                        
+                        rowOutput[j * PixelSize + 2] = c;
+                        rowOutput[j * PixelSize + 1] = c;
+                        rowOutput[j * PixelSize] = c;
                     }
-
                 }
             }
             src.UnlockBits(bdSrc);
