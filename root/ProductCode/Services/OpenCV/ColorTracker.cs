57d042d451e66684ec3987acd1568ba50cd50392
deaddog <mikkelslsl@gmail.com>
2013-11-13 09:50:43 +0100

    Added methods for determining distance between points and colors (for automated calibration).

--- root/ProductCode/Services/OpenCV/ColorTracker.cs
+++ root/ProductCode/Services/OpenCV/ColorTracker.cs
@@ -24,7 +24,6 @@ namespace TrackColorForm
             this.targetColor = color;
         }
 
-
         public float Threshold
         {
             get { return threshold; }
@@ -74,6 +73,22 @@ namespace TrackColorForm
                 center = new PointF(bounds.X + bounds.Width / 2f, bounds.Y + bounds.Height / 2f);
                 return true;
             }
+        }
+
+        private double distance(Color c1, Color c2)
+        {
+            double r = c1.R - c2.R;
+            double g = c1.G - c2.G;
+            double b = c1.B - c2.B;
+
+            return Math.Sqrt(r * r + g * g + b * b);
+        }
+        private double distance(PointF p1, PointF p2)
+        {
+            double x = p1.X - p2.X;
+            double y = p1.Y - p2.Y;
+
+            return Math.Sqrt(x * x + y * y);
         }
     }
 }
