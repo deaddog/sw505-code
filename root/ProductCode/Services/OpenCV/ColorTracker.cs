724c6376c594a4aa71de020b2b800261d1097836
deaddog <mikkelslsl@gmail.com>
2013-11-13 09:47:56 +0100

    Added fields and properties for controlling threshold, target and center.

--- root/ProductCode/Services/OpenCV/ColorTracker.cs
+++ root/ProductCode/Services/OpenCV/ColorTracker.cs
@@ -8,7 +8,38 @@ using System.Drawing.Imaging;
 
 namespace TrackColorForm
 {
-    public static class ColorTracker
+    public class ColorTracker
     {
+        private const float DEFAULT_THRESHOLD = 50;
+
+        private float threshold;
+        private Color targetColor;
+        private PointF center;
+
+        private DateTime lastUpdate;
+
+        public ColorTracker(Color color)
+        {
+            this.threshold = DEFAULT_THRESHOLD;
+            this.targetColor = color;
+        }
+
+        public float Threshold
+        {
+            get { return threshold; }
+        }
+        public Color Color
+        {
+            get { return targetColor; }
+            set
+            {
+                this.targetColor = value;
+                threshold = DEFAULT_THRESHOLD;
+            }
+        }
+        public PointF Center
+        {
+            get { return center; }
+        }
     }
 }
