389425d481eddb4b030eb3c7c12035d8b51a278c
deaddog <mikkelslsl@gmail.com>
2013-11-14 14:37:40 +0100

    Added documentation for the operators defined in Vector2D.

--- root/ProductCode/CommonLib/DTOs/Vector2D.cs
+++ root/ProductCode/CommonLib/DTOs/Vector2D.cs
@@ -53,35 +53,79 @@ namespace CommonLib.DTOs
             this.y = y;
         }
 
+        /// <summary>
+        /// Joins two vectors using addition.
+        /// </summary>
+        /// <param name="v1">The first vector in the addition.</param>
+        /// <param name="v2">The second vector in the addition.</param>
+        /// <returns>A new <see cref="Vector2D"/> which is the sum of the two parameter vectors.</returns>
         public static Vector2D operator +(Vector2D v1, Vector2D v2)
         {
             return new Vector2D(v1.x + v2.x, v1.y + v2.y);
         }
+        /// <summary>
+        /// Joins two vectors using subtraction.
+        /// </summary>
+        /// <param name="v1">The first vector in the subtraction.</param>
+        /// <param name="v2">The second vector in the subtraction.</param>
+        /// <returns>A new <see cref="Vector2D"/> which is the difference between the two parameter vectors.</returns>
         public static Vector2D operator -(Vector2D v1, Vector2D v2)
         {
             return new Vector2D(v1.x - v2.x, v1.y - v2.y);
         }
+        /// <summary>
+        /// Multiplies a vector with a scale value.
+        /// </summary>
+        /// <param name="v1">The vector.</param>
+        /// <param name="scale">The scale.</param>
+        /// <returns>A new <see cref="Vector2D"/> where both coordinates are multiplied by the scale value.</returns>
         public static Vector2D operator *(Vector2D v1, float scale)
         {
             return new Vector2D(v1.x * scale, v1.y * scale);
         }
+        /// <summary>
+        /// Divides a vector with a scale value.
+        /// </summary>
+        /// <param name="v1">The vector.</param>
+        /// <param name="scale">The scale.</param>
+        /// <returns>A new <see cref="Vector2D"/> where both coordinates are divided by the scale value.</returns>
         public static Vector2D operator /(Vector2D v1, float scale)
         {
             return new Vector2D(v1.x / scale, v1.y / scale);
         }
 
+        /// <summary>
+        /// Converts a <see cref="Vector2D"/> to a <see cref="System.Drawing.PointF"/>.
+        /// </summary>
+        /// <param name="vector">The vector to convert.</param>
+        /// <returns>A new <see cref="System.Drawing.PointF"/> with coordinates equal to the coordinates of the vector.</returns>
         public static explicit operator System.Drawing.PointF(Vector2D vector)
         {
             return new System.Drawing.PointF(vector.x, vector.y);
         }
+        /// <summary>
+        /// Converts a <see cref="Vector2D"/> to a <see cref="System.Drawing.SizeF"/>.
+        /// </summary>
+        /// <param name="vector">The vector to convert.</param>
+        /// <returns>A new <see cref="System.Drawing.SizeF"/> with size equal to the coordinates of the vector.</returns>
         public static explicit operator System.Drawing.SizeF(Vector2D vector)
         {
             return new System.Drawing.SizeF(vector.x, vector.y);
         }
+        /// <summary>
+        /// Converts a <see cref="System.Drawing.PointF"/> to a <see cref="Vector2D"/>.
+        /// </summary>
+        /// <param name="point">The point to convert.</param>
+        /// <returns>A new <see cref="Vector2D"/> with coordinates equal to the coordinates of the point.</returns>
         public static explicit operator Vector2D(System.Drawing.PointF point)
         {
             return new Vector2D(point.X, point.Y);
         }
+        /// <summary>
+        /// Converts a <see cref="System.Drawing.SizeF"/> to a <see cref="Vector2D"/>.
+        /// </summary>
+        /// <param name="point">The point to convert.</param>
+        /// <returns>A new <see cref="Vector2D"/> with coordinates equal to the size of the 'size'.</returns>
         public static explicit operator Vector2D(System.Drawing.SizeF size)
         {
             return new Vector2D(size.Width, size.Height);
