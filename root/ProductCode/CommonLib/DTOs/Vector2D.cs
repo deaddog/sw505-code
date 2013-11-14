ac67fc067ad2f0278866a578e818bcf8ed0b4f83
deaddog <mikkelslsl@gmail.com>
2013-11-14 13:59:53 +0100

    Provided a simple implementation of Vector2D, including a few operator overloads.

--- root/ProductCode/CommonLib/DTOs/Vector2D.cs
+++ root/ProductCode/CommonLib/DTOs/Vector2D.cs
@@ -7,5 +7,31 @@ namespace CommonLib.DTOs
     /// </summary>
     public struct Vector2D
     {
+        private float x, y;
+
+        public Vector2D(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vector2D operator +(Vector2D v1, Vector2D v2)
+        {
+            return new Vector2D(v1.x + v2.x, v1.y + v2.y);
+        }
+        public static Vector2D operator -(Vector2D v1, Vector2D v2)
+        {
+            return new Vector2D(v1.x - v2.x, v1.y - v2.y);
+        }
+        public static Vector2D operator *(Vector2D v1, float scale)
+        {
+            return new Vector2D(v1.x * scale, v1.y * scale);
+        }
+
+        public Vector2D Normalize()
+        {
+            double len = Math.Sqrt(x * x + y * y);
+            return new Vector2D((float)(x / len), (float)(y / len));
+        }
     }
 }
