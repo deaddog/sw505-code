3cac64a6fad0f67d6204071024116189ff8d11e7
deaddog <mikkelslsl@gmail.com>
2013-11-14 14:26:05 +0100

    Provided documentation for Vector2D.

--- root/ProductCode/CommonLib/DTOs/Vector2D.cs
+++ root/ProductCode/CommonLib/DTOs/Vector2D.cs
@@ -7,20 +7,46 @@ namespace CommonLib.DTOs
     /// </summary>
     public struct Vector2D
     {
-        private float x, y;
-
         private static Vector2D zero = new Vector2D(0, 0);
         private static Vector2D one = new Vector2D(1, 1);
 
+        /// <summary>
+        /// Gets a <see cref="Vector2D"/> with X and Y both equal to 0.
+        /// </summary>
         public static Vector2D Zero
         {
             get { return zero; }
         }
+        /// <summary>
+        /// Gets a <see cref="Vector2D"/> with X and Y both equal to 1.
+        /// </summary>
         public static Vector2D One
         {
             get { return one; }
         }
 
+        private float x, y;
+
+        /// <summary>
+        /// Gets the X-coordinate.
+        /// </summary>
+        public float X
+        {
+            get { return x; }
+        }
+        /// <summary>
+        /// Gets the Y-coordinate.
+        /// </summary>
+        public float Y
+        {
+            get { return y; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Vector2D"/> struct.
+        /// </summary>
+        /// <param name="x">The x-coordinate.</param>
+        /// <param name="y">The x-coordinate.</param>
         public Vector2D(float x, float y)
         {
             this.x = x;
@@ -44,6 +70,10 @@ namespace CommonLib.DTOs
             return new Vector2D(v1.x / scale, v1.y / scale);
         }
 
+        /// <summary>
+        /// Normalizes this <see cref="Vector2D"/>.
+        /// </summary>
+        /// <returns>A new <see cref="Vector2D"/> with lenght 1.</returns>
         public Vector2D Normalize()
         {
             double len = Math.Sqrt(x * x + y * y);
