8a1ccb01e2e2706ba01fba3af37110730c54bc6f
mikaelec <mikaelec@gmail.com>
2013-11-15 09:54:36 +0100

    PointConverter.cs tilføjet til Services:   konverterer Vector2D til Coordinate (som er en ny struct)

---
+++ root/ProductCode/CommonLib/DTOs/Coordinate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib.DTOs
+{
+    public struct Coordinate
+    {
+        private float x, y;
+
+        public float X { get { return x; } }
+
+        public float Y { get { return y; } }
+
+        public Coordinate(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public string Encode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ root/ProductCode/Services/PointConverter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonLib.DTOs;
+
+namespace Services
+{
+    public class PointConverter
+    {
+        private int imageSizeX, imageSizeY;
+        private float cameraAngleX, cameraAngleY, actualSizeX, actualSizeY;
+        public PointConverter(int imageSizeX, int imageSizeY, float cameraAngleX,
+            float cameraAngleY, float actualSizeX, float actualSizeY)
+        {
+            this.imageSizeX = imageSizeX;
+            this.imageSizeY = imageSizeY;
+            this.cameraAngleX = cameraAngleX;
+            this.cameraAngleY = cameraAngleY;
+            this.actualSizeX = actualSizeX;
+            this.actualSizeY = actualSizeY;
+        }
+
+        public Coordinate ConvertPointToCoordinate(Vector2D point)
+        {
+            float oldx = point.X;
+            float oldy = point.Y;
+            float wideAngleX = calcWideAngle(this.cameraAngleX);
+            float wideAngleY = calcWideAngle(this.cameraAngleY);
+
+            float cameraToActualDist = calcDist(actualSizeX, wideAngleX);
+            float cameraToImageDist = calcDist(imageSizeX, wideAngleX);
+
+            float angleX = calcAngle(cameraToImageDist, oldx);
+            float angleY = calcAngle(cameraToImageDist, oldy);
+
+            float x = calcActual(angleX, cameraToActualDist);
+            float y = calcActual(angleY, cameraToActualDist);
+            return new Coordinate(x, y);
+        }
+
+        private float calcWideAngle(float cameraAngle)
+        {
+            return 90 - (cameraAngle / 2);
+        }
+
+        private float calcDist(float sizeX, float wideAngleX)
+        {
+            return (sizeX / (float)Math.Sin(degToRad(this.cameraAngleX / 2))) * (float)Math.Sin(degToRad(wideAngleX));
+        }
+
+        private float calcAngle(float cameraToImageDist, float pointDim)
+        {
+            return (float)Math.Tanh(degToRad(pointDim / cameraToImageDist));
+        }
+
+        private float calcActual(float angle, float cameraToActualDist)
+        {
+            return (float)Math.Tanh(degToRad(angle)) * cameraToActualDist;
+        }
+
+        private float degToRad(float degrees)
+        {
+            return degrees * ((float)Math.PI / 180);
+        }
+    }
+}
