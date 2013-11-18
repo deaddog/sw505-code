38a01d26430b1a5b43b1b69d4161e3de40005bb4
deaddog <mikkelslsl@gmail.com>
2013-11-18 09:51:22 +0100

    Update PointConverter to the simple model.

--- root/ProductCode/Services/TrackingServices/PointConverter.cs
+++ root/ProductCode/Services/TrackingServices/PointConverter.cs
@@ -8,60 +8,20 @@ namespace Services.TrackingServices
 {
     public class PointConverter
     {
-        private int imageSizeX, imageSizeY;
-        private float cameraAngleX, cameraAngleY, actualSizeX, actualSizeY;
-        public PointConverter(int imageSizeX, int imageSizeY, float cameraAngleX,
-            float cameraAngleY, float actualSizeX, float actualSizeY)
+        private readonly float actualSizeX, actualSizeY;
+        private readonly float scaleX, scaleY;
+
+        public PointConverter(int imageSizeX, int imageSizeY, float actualSizeX, float actualSizeY)
         {
-            this.imageSizeX = imageSizeX;
-            this.imageSizeY = imageSizeY;
-            this.cameraAngleX = cameraAngleX;
-            this.cameraAngleY = cameraAngleY;
-            this.actualSizeX = actualSizeX;
-            this.actualSizeY = actualSizeY;
+            this.actualSizeX = actualSizeX / 2f;
+            this.actualSizeY = actualSizeY / 2f;
+            this.scaleX = actualSizeX / ((float)imageSizeX / 2f);
+            this.scaleY = actualSizeY / ((float)imageSizeY / 2f);
         }
 
         public Coordinate ConvertPointToCoordinate(Vector2D point)
         {
-            float oldx = point.X;
-            float oldy = point.Y;
-            float wideAngleX = calcWideAngle(this.cameraAngleX);
-            float wideAngleY = calcWideAngle(this.cameraAngleY);
-
-            float cameraToActualDist = calcDist(actualSizeX, wideAngleX);
-            float cameraToImageDist = calcDist(imageSizeX, wideAngleX);
-
-            float angleX = calcAngle(cameraToImageDist, oldx - (imageSizeX / 2));
-            float angleY = calcAngle(cameraToImageDist, oldy - (imageSizeY / 2));
-
-            float x = calcActual(angleX, cameraToActualDist);
-            float y = calcActual(angleY, cameraToActualDist);
-            return new Coordinate(x, y);
-        }
-
-        private float calcWideAngle(float cameraAngle)
-        {
-            return 90 - (cameraAngle / 2);
-        }
-
-        private float calcDist(float sizeX, float wideAngleX)
-        {
-            return ((sizeX / 2) / (float)Math.Sin(degToRad(this.cameraAngleX / 2))) * (float)Math.Sin(degToRad(wideAngleX));
-        }
-
-        private float calcAngle(float cameraToImageDist, float pointDim)
-        {
-            return (float)Math.Tanh(pointDim / cameraToImageDist);
-        }
-
-        private float calcActual(float angle, float cameraToActualDist)
-        {
-            return (float)Math.Tanh(angle) * cameraToActualDist;
-        }
-
-        private float degToRad(float degrees)
-        {
-            return degrees * ((float)Math.PI / 180);
+            return new Coordinate(scaleX * point.X - actualSizeX, scaleY * point.Y - actualSizeY);
         }
     }
 }
