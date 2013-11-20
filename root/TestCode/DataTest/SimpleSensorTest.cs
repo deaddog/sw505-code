d807f0b53b2f3354b04e5eff0286bbaecc6e287e
Bruno Thalmann <mr.thalmann@gmail.com>
2013-11-20 11:00:26 +0100

    started on unit test for sensormodel

---
+++ root/TestCode/DataTest/SimpleSensorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonLib.Interfaces;
+using Services.RobotServices;
+using Data;
+using CommonLib.DTOs;
+
+namespace DataTest
+{
+    [TestClass]
+    public class SimpleSensorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var robot = RobotFactory.GetInstance().CreateRobot();
+            var sensor = new SensorDataDTO();
+            var sensorModel = SensorModelFactory.GetInstance().CreateSimpleSensorModel();
+
+            //sensorModel.GetProbabilityUltrasonicSensorX(robot.
+        }
+    }
+}
