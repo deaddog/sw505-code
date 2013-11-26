b21fb69295113daf892f890d8ba2af6e9571f284
cyborbob <rdf_errorzone@hotmail.com>
2013-11-26 10:50:54 +0100

    Added NXTPostMan Interface and namespace

---
+++ root/ProductCode/CommonLib/NXTPostMan/INXTPostMan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CommonLib.NXTPostMan
+{
+    public interface INXTPostMan
+    {
+
+        void SendMessage(INXTMessage msg);
+
+        bool HasMessageArrived(IMessageType type);
+
+        bool HasMessageArrived(string msg);
+
+
+    }
+
+    public interface IMessageType { }
+    public interface INXTMessage { }
+}
