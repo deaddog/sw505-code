352c1be04aed8e0844f1432acc2723b5c2e71e17
cyborbob <rdf_errorzone@hotmail.com>
2013-11-26 11:23:36 +0100

    Added unittest for PostMan Class.

---
+++ root/TestCode/CommonLibTest/PostManTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonLib;
+using CommonLib.Interfaces;
+using CommonLib.DTOs;
+using CommonLib.NXTPostMan;
+
+namespace CommonLibTest
+{
+    [TestClass]
+    public class PostManTest
+    {
+
+        private static TestContext context;
+        
+        #region Initializers
+
+        [ClassInitialize]
+        public static void ClassInitializer(TestContext c)
+        {
+            context = c;
+        }
+
+        [ClassCleanup]
+        public static void ClassCleaner()
+        {
+            context = null;
+        }
+
+        [TestInitialize]
+        public void Initializer()
+        {
+ 
+        }
+
+        [TestCleanup]
+        public void Cleaner()
+        {
+            // cleanup after test.
+  
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void Template_StateUnderTest_ExpectedResult()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+
+            Assert.Inconclusive();
+        }
+
+
+
+    }
+}
