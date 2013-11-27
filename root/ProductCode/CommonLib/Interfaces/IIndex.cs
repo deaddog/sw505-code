78ca5c2c8c7e77e880096a24518992ebe39144e6
stufkan <stufkan@gmail.com>
2013-11-27 15:13:22 +0100

    Coordinate and Index DTOs

---
+++ root/ProductCode/CommonLib/Interfaces/IIndex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib.Interfaces
+{
+    public interface IIndex
+    {
+        int X { get; }
+        int Y { get; }
+    }
+}
