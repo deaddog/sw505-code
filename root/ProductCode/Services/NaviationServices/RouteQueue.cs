4b377166fab05692e5665511b8308dbba66f59d1
stufkan <stufkan@gmail.com>
2013-11-28 10:11:10 +0100

    Implemented a RouteQueue to handle a queue of coordinates

---
+++ root/ProductCode/Services/NaviationServices/RouteQueue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.NaviationServices
+{
+    public class RouteQueue
+    {
+        private Queue<
+
+    }
+}
