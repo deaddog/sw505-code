62dc1860d2fad827454d21d3c71f09217690caf4
deaddog <mikkelslsl@gmail.com>
2013-12-02 10:43:46 +0100

    Implemented automatic update of RgbStream using Kinect event. RgbStream exposed through Singleton pattern.

--- root/ProductCode/Services/KinectServices/RgbStream.cs
+++ root/ProductCode/Services/KinectServices/RgbStream.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
@@ -9,15 +10,71 @@ namespace Services.KinectServices
 {
     public class RgbStream
     {
+        private const int CONNECT_SLEEP_TIME = 1000;
+
         private KinectSensor kinectSensor;
         private Bitmap currentImage;
         private object updaterLock;
+
+        private Bitmap lastBitmap;
+        private bool hasBeenLoaded;
+        public Bitmap Bitmap
+        {
+            get { lock (updaterLock) { hasBeenLoaded = true; return lastBitmap; } }
+        }
+
+        private static RgbStream instance;
+        public static RgbStream Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    KinectSensor sensor;
+
+                    var sensors = from s in KinectSensor.KinectSensors
+                                  where s.Status == KinectStatus.Connected
+                                  select s;
+
+                    while ((sensor = sensors.FirstOrDefault()) == null)
+                        System.Threading.Thread.Sleep(CONNECT_SLEEP_TIME);
+
+                    sensor.Start();
+
+                    instance = new RgbStream(sensor);
+                }
+
+                return instance;
+            }
+        }
 
         private RgbStream(KinectSensor sensor)
         {
             this.kinectSensor = sensor;
             this.currentImage = null;
             this.updaterLock = new object();
+
+            //Start the sensor and wait for it to be ready
+            this.kinectSensor.Start();
+            while (!this.kinectSensor.IsRunning) { }
+
+            //Event for when a frame from the colorstream is ready
+            this.kinectSensor.ColorFrameReady += (s, e) =>
+            {
+                lock (updaterLock)
+                {
+                    if (!hasBeenLoaded && lastBitmap != null)
+                    {
+                        lastBitmap.Dispose();
+                        lastBitmap = null;
+                    }
+
+                    using (var frame = e.OpenColorImageFrame())
+                        lastBitmap = ImageToBitmap(frame);
+
+                    hasBeenLoaded = false;
+                }
+            };
         }
 
         private static Bitmap ImageToBitmap(ColorImageFrame Image)
