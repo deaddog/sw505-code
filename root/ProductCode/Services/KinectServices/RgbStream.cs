cc2242d9d5b16d2857e66de8e58551a91309dc24
deaddog <mikkelslsl@gmail.com>
2013-12-02 09:18:32 +0100

    Added ImageToBitmap method for converting kinect images to System.Drawing.Bitmap's.

--- root/ProductCode/Services/KinectServices/RgbStream.cs
+++ root/ProductCode/Services/KinectServices/RgbStream.cs
@@ -1,9 +1,34 @@
-﻿using System;
+﻿using Microsoft.Kinect;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Services.KinectServices
 {
     public class RgbStream
     {
+        private static Bitmap ImageToBitmap(ColorImageFrame Image)
+        {
+            if (Image != null)
+            {
+                byte[] pixeldata = new byte[Image.PixelDataLength];
+                Image.CopyPixelDataTo(pixeldata);
+                Bitmap bmap = new Bitmap(Image.Width, Image.Height, PixelFormat.Format32bppRgb);
+                BitmapData bmapdata = bmap.LockBits(
+                    new Rectangle(0, 0, Image.Width, Image.Height),
+                    ImageLockMode.WriteOnly,
+                    bmap.PixelFormat);
+                IntPtr ptr = bmapdata.Scan0;
+                Marshal.Copy(pixeldata, 0, ptr, Image.PixelDataLength);
+                bmap.UnlockBits(bmapdata);
+                return bmap;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
