64f2fd53a3b697c3dae5efbd13b43af328048a4e
deaddog <mikkelslsl@gmail.com>
2013-12-03 09:24:38 +0100

    Added DisplayControl, which manages a stream of images.

---
+++ root/ProductCode/Control/DisplayControl.cs
@@ -0,0 +1,42 @@
+﻿using Services.KinectServices;
+using System;
+using System.Drawing;
+
+namespace Control
+{
+    public class DisplayControl
+    {
+        private RgbStream stream;
+
+        private static DisplayControl instance;
+        public static DisplayControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DisplayControl();
+                }
+                return instance;
+            }
+        }
+
+        private DisplayControl()
+        {
+            this.stream = RgbStream.Instance;
+            this.stream.ImageFormat = RgbImageFormats.Resolution640x480Fps30;
+
+            this.stream.ImageUpdated += (s, e) =>
+            {
+                if (ImageUpdated != null)
+                    ImageUpdated(this, EventArgs.Empty);
+            };
+        }
+
+        public event EventHandler ImageUpdated;
+        public Bitmap Bitmap
+        {
+            get { return stream.Bitmap; }
+        }
+    }
+}
