33cde1ecf22f3c24309480d95ef35ebd01f79cb8
deaddog <mikkelslsl@gmail.com>
2013-12-03 10:13:37 +0100

    Added a OrientationTracker to LocationControl and properties to set its colors.

--- root/ProductCode/Control/LocationControl.cs
+++ root/ProductCode/Control/LocationControl.cs
@@ -6,18 +6,36 @@ using System.Drawing;
 
 namespace Control
 {
+    /// <summary>
+    /// Handles locating a robot using color tracking.
+    /// </summary>
     public class LocationControl
     {
         private static readonly IPose defaultPose = new Pose(0.0f, 0.0f, 0.0);
 
+        private OrientationTracker tracker;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="LocationControl"/> class.
         /// </summary>
-        public LocationControl() : this(new OrientationTracker(Color.Black, Color.White)) { }
-
+        public LocationControl()
+            : this(new OrientationTracker(Color.Black, Color.White))
+        {
+        }
         private LocationControl(OrientationTracker tracker)
         {
+            this.tracker = tracker;
+        }
 
+        public Color FrontColor
+        {
+            get { return tracker.FrontColor; }
+            set { tracker.FrontColor = value; }
+        }
+        public Color RearColor
+        {
+            get { return tracker.RearColor; }
+            set { tracker.RearColor = value; }
         }
 
         /// <summary>
