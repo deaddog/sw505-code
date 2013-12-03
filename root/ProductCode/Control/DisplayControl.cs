e2bf1de921e3191742941d87f2db254292f2ea71
deaddog <mikkelslsl@gmail.com>
2013-12-03 09:28:24 +0100

    Commented the DisplayControl.

--- root/ProductCode/Control/DisplayControl.cs
+++ root/ProductCode/Control/DisplayControl.cs
@@ -4,11 +4,17 @@ using System.Drawing;
 
 namespace Control
 {
+    /// <summary>
+    /// Retrieves rgb images from a Microsoft Kinect.
+    /// </summary>
     public class DisplayControl
     {
         private RgbStream stream;
 
         private static DisplayControl instance;
+        /// <summary>
+        /// Gets the singleton instance of <see cref="DisplayControl"/>.
+        /// </summary>
         public static DisplayControl Instance
         {
             get
@@ -33,10 +39,26 @@ namespace Control
             };
         }
 
+        /// <summary>
+        /// Occurs when the <see cref="DisplayControl.Bitmap"/> property is updated with a new image.
+        /// After this event occurs, the previous image is automatically disposed.
+        /// </summary>
         public event EventHandler ImageUpdated;
+        /// <summary>
+        /// Gets the last bitmap captured.
+        /// </summary>
         public Bitmap Bitmap
         {
             get { return stream.Bitmap; }
+        }
+
+        /// <summary>
+        /// Gets or sets the image format used by this <see cref="DisplayControl"/>.
+        /// </summary>
+        public RgbImageFormats ImageFormat
+        {
+            get { return stream.ImageFormat; }
+            set { stream.ImageFormat = value; }
         }
     }
 }
