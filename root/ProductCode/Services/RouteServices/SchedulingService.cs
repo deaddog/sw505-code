7cabcccca8a9f7e3cfd63482f3ff48c1ec9496b2
stufkan <stufkan@gmail.com>
2013-12-05 09:26:53 +0100

    Initial methods in scheduling service

---
+++ root/ProductCode/Services/RouteServices/SchedulingService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLib.Interfaces;
+using Data;
+
+namespace Services.RouteServices
+{
+    public class SchedulingService
+    {
+        private static SchedulingService instance;
+
+        private SchedulingService()
+        {
+
+        }
+
+        public static SchedulingService Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SchedulingService();
+                return instance;
+            }
+        }
+        public IEnumerable<ICoordinate> GetRoute(OccupancyGrid grid)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
