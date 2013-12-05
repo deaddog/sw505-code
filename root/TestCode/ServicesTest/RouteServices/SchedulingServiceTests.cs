614d9916e1da2dab0a4467246a077a8ace045617
stufkan <stufkan@gmail.com>
2013-12-05 13:07:49 +0100

    SchedulingService test

---
+++ root/TestCode/ServicesTest/RouteServices/SchedulingServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.RouteServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data;
+namespace Services.RouteServices.Tests
+{
+    [TestClass()]
+    public class SchedulingServiceTests
+    {
+        private SchedulingService s;
+
+        [TestInitialize]
+        public void Initializer()
+        {
+            s = SchedulingService.Instance;
+        }
+
+        [TestMethod()]
+        public void GetRouteTest()
+        {
+            OccupancyGrid g = new OccupancyGrid(30, 23, 10, -150, -115);
+            s.GetRoute(g);
+
+
+            Assert.Fail();
+        }
+    }
+}
