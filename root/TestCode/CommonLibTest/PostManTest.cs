41984ccc35f313c85537d3644ba26c64611ee4be
stufkan <stufkan@gmail.com>
2013-12-11 08:50:45 +0100

    Removed old SendMessage overload

--- root/TestCode/CommonLibTest/PostManTest.cs
+++ root/TestCode/CommonLibTest/PostManTest.cs
@@ -78,37 +78,5 @@ namespace CommonLibTest
             Assert.IsTrue(result == DialogResult.Yes);
         }
 
-
-        [TestMethod]
-        public void SendMessage_RobotReadyAndAbleCoordinateAsMessage_MessageRecievedByRobot()
-        {
-            // Arrange
-            ICoordinate cord = new Vector2D(5.0f, 5.0f);
-            DialogResult result;
-
-            // Act
-            ((PostMan)postman).SendMessage(cord);
-            result = MessageBox.Show("Did the robot recieve move to command ?", "Test Result", MessageBoxButtons.YesNo);
-
-            // Assert
-            Assert.IsTrue(result == DialogResult.Yes);
-        }
-
-        [TestMethod]
-        public void HasMessageArrived_MessageHasArrived_ReturnTrue()
-        {
-            // Arrange
-            ICoordinate cord = new Vector2D(50.0f, 50.0f);
-            ((PostMan)postman).SendMessage(cord);
-
-            // Act
-            MessageBox.Show("Press OK when message from robot has arrived?", "User Input", MessageBoxButtons.OK);
-            bool result = postman.HasMessageArrived(NXTMessageType.RobotRequestsLocation);
-
-            // Assert
-            Assert.IsTrue(result);
-        }
-
-
     }
 }
