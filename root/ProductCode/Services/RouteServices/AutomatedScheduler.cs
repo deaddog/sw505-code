5b83b314ce58dcab280304ca742e41ba9d8fbdd2
deaddog <mikkelslsl@gmail.com>
2013-12-12 14:47:06 +0100

    AutomatedScheduler defines methods for translating cells to coordinates and back. This is used to implement scheduling using CellIndex only.

--- root/ProductCode/Services/RouteServices/AutomatedScheduler.cs
+++ root/ProductCode/Services/RouteServices/AutomatedScheduler.cs
@@ -1,4 +1,5 @@
-﻿using CommonLib.Interfaces;
+﻿using CommonLib.DTOs;
+using CommonLib.Interfaces;
 using Data;
 
 using System;
@@ -15,7 +16,25 @@ namespace Services.RouteServices
 
         public IEnumerable<ICoordinate> GetRoute(IPose robotLocation, OccupancyGrid grid)
         {
+            foreach (CellIndex index in getRoute(robotLocation, grid))
+                yield return getCellCenter(index, grid);
+        }
+
+        private IEnumerable<CellIndex> getRoute(IPose robotLocation, OccupancyGrid grid)
+        {
             throw new NotImplementedException();
+        }
+
+        private static CellIndex getIndex(ICoordinate pose, OccupancyGrid grid)
+        {
+            int robotCellX = (int)Math.Floor((pose.X - grid.X) / grid.CellSize);
+            int robotCellY = (int)Math.Floor((pose.Y - grid.Y) / grid.CellSize);
+            return new CellIndex(robotCellX, robotCellY);
+        }
+        private static ICoordinate getCellCenter(CellIndex index, OccupancyGrid grid)
+        {
+            float cellRadius = grid.CellSize / 2;
+            return new CellCoordinate(grid.X + grid.CellSize * index.X + cellRadius, grid.Y + grid.CellSize * index.Y + cellRadius);
         }
     }
 }
