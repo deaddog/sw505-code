f7922e3268431238c08c0a3d58df99ddf1e7fa55
deaddog <mikkelslsl@gmail.com>
2013-12-12 13:24:26 +0100

    Create IRouter interface for implementing route services.

---
+++ root/ProductCode/Services/RouteServices/IRouter.cs
@@ -0,0 +1,21 @@
+﻿using CommonLib.Interfaces;
+using Data;
+
+using System.Collections.Generic;
+
+namespace Services.RouteServices
+{
+    /// <summary>
+    /// Defines a route service, capable of determining a route for a robot.
+    /// </summary>
+    public interface IRouter
+    {
+        /// <summary>
+        /// Gets the next route for a robot, such that the robot scans at the end of the route.
+        /// </summary>
+        /// <param name="robotLocation">The current robot location.</param>
+        /// <param name="grid">The grid in which the robot moves.</param>
+        /// <returns>A collection of <see cref="ICoordinate"/>s through which the robot should move.</returns>
+        IEnumerable<ICoordinate> GetRoute(IPose robotLocation, OccupancyGrid grid);
+    }
+}
