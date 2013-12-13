3abe70ce41ae631893c290f1f1205c9b770e0944
stufkan <stufkan@gmail.com>
2013-12-13 10:26:35 +0100

    Implemented gaussian sensor model and created a factory method for it

--- root/ProductCode/Data/SensorModel/GaussianSensorModel.cs
+++ root/ProductCode/Data/SensorModel/GaussianSensorModel.cs
@@ -4,22 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommonLib.DTOs;
+using CommonLib;
 
 namespace Data.SensorModel
 {
     public class GaussianSensorModel : AbstractSensorModel
     {
         protected const double ETA = 1;
+        protected const double RHO = 0.1;
+        protected double constantdenominator = 2 * Math.Pow(AVERAGE_OBSTACLE_DEPTH_CM / 2, 2);
+        protected double constantfactor = 1 /(Math.Sqrt(2 * Math.PI * Math.Pow((AVERAGE_OBSTACLE_DEPTH_CM / 6), 2)));
 
         public double NormalDistribution(byte sensor, double distance)
         {
-            return 1 /
-                (Math.Sqrt(2 * Math.PI * Math.Pow((AVERAGE_OBSTACLE_DEPTH_CM / 6), 2)))
-
-                * Math.Pow(Math.E,
-                -(Math.Pow((distance - sensor), 2)
-                /
-                (Math.Pow(AVERAGE_OBSTACLE_DEPTH_CM, 2) / 6)));
+            return 
+                constantfactor * Math.Pow(Math.E,-(Math.Pow((distance - sensor), 2)/ constantdenominator ));
         }
 
         public override double GetProbability(OccupancyGrid grid, CommonLib.Interfaces.IPose robot, CommonLib.DTOs.CellIndex cell, byte sensorX)
@@ -32,10 +31,13 @@ namespace Data.SensorModel
 
             if (r < MINIMIM_SENSOR_RANGE_CM)
                 return NEAR_CELL_PROBABILITY;
-            else if (r > Math.Min(MAXIMUM_SENSOR_RANGE_CM, sensorX + AVERAGE_OBSTACLE_DEPTH_CM/2))
+            else if (r > Math.Min(MAXIMUM_SENSOR_RANGE_CM, sensorX + AVERAGE_OBSTACLE_DEPTH_CM / 2))
                 return initialProbability;
-            else if (sensorX - AVERAGE_OBSTACLE_DEPTH_CM/2 <= r && r <= sensorX + AVERAGE_OBSTACLE_DEPTH_CM/2)
-                throw new NotImplementedException();
+            else if (sensorX - AVERAGE_OBSTACLE_DEPTH_CM / 2 <= r && r <= sensorX + AVERAGE_OBSTACLE_DEPTH_CM / 2)
+            {
+                Func<double, double> NormalDist = x => NormalDistribution(sensorX, x);
+                return ETA * ExtendedMath.DefIntegrate(NormalDist, r - RHO, r + RHO);
+            }
             else
                 return FREE_CELL_PROBABILITY;
         }
