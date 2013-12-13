51a69a383dc88a0ab0d7abdb0556797a901cfafa
deaddog <mikkelslsl@gmail.com>
2013-12-13 09:00:26 +0100

    Implemented the DijkstraNode using a weight, a parent and a value.

--- root/ProductCode/Services/RouteServices/Automation/DijkstraNode.cs
+++ root/ProductCode/Services/RouteServices/Automation/DijkstraNode.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Services.RouteServices.Automation
 {
     public class DijkstraNode<T>
     {
+        private uint weight;
+        private DijkstraNode<T> parent;
+        private T value;
+
+        public DijkstraNode(T value)
+        {
+            this.weight = uint.MaxValue;
+            this.parent = null;
+            this.value = value;
+        }
+
+        public uint Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public DijkstraNode<T> Parent
+        {
+            get { return parent; }
+            set { parent = value; }
+        }
+        public T Value
+        {
+            get { return value; }
+        }
     }
 }
