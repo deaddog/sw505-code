962a418ce1fd59cb2dfcde5220aaf260ddb00dfc
deaddog <mikkelslsl@gmail.com>
2013-12-14 17:22:55 +0100

    Added DijkstraScheduler which will handle the automated part of the automated sheduler.

---
+++ root/ProductCode/Services/RouteServices/Automation/DijkstraScheduler.cs
@@ -0,0 +1,16 @@
+﻿using CommonLib.DTOs;
+using CommonLib.Interfaces;
+using Data;
+using System;
+using System.Collections.Generic;
+
+namespace Services.RouteServices.Automation
+{
+    public class DijkstraScheduler : ICellScheduler
+    {
+        public IEnumerable<CellIndex> GetIndexRoute(IPose robotLocation, OccupancyGrid grid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
