400f673f6e2d5337506ef8a3be52a6ea67349ed1
deaddog <mikkelslsl@gmail.com>
2013-12-14 23:27:12 +0100

    Removed cell depth from abstract sensor model. This should be read from the grid.

--- root/ProductCode/Data/SensorModel/AbstractSensorModel.cs
+++ root/ProductCode/Data/SensorModel/AbstractSensorModel.cs
@@ -23,30 +23,9 @@ namespace Data.SensorModel
         /// Initializes a new instance of the <see cref="SimpleSensorModel"/> class.
         /// </summary>
         /// <param name="cellDepthCM">The cell depth cm.</param>
-        public AbstractSensorModel(double cellDepthCM = 20)
+        public AbstractSensorModel()
         {
-            this.cellDepthCM = cellDepthCM;
             this.initialProbability = OccupancyGrid.INITIAL_PROBABILITY;
-        }
-
-
-        protected double cellDepthCM;
-
-        /// <summary>
-        /// Gets the cell depth cm.
-        /// </summary>
-        /// <value>
-        /// The cell depth cm.
-        /// </value>
-        public double CellDepthCM
-        {
-            get { return cellDepthCM; }
-        }
-
-        protected CellCoordinate getCoordinateFromCellIndex(OccupancyGrid grid, CellIndex index)
-        {
-            float cellRadius = grid.CellSize / 2;
-            return new CellCoordinate(grid.X + grid.CellSize * index.X + cellRadius, grid.Y + grid.CellSize * index.Y + cellRadius);
         }
 
         public abstract double GetProbability(OccupancyGrid grid, CommonLib.Interfaces.IPose robot, CellIndex cell, byte sensorX);
