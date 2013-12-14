719f0b20f94528393c292db4ade0462249156f55
deaddog <mikkelslsl@gmail.com>
2013-12-14 17:21:21 +0100

    InitialScheduler handles all scans related to performing the setup scans for the automated scheduler.

--- root/ProductCode/Services/RouteServices/Automation/InitialScheduler.cs
+++ root/ProductCode/Services/RouteServices/Automation/InitialScheduler.cs
@@ -7,9 +7,40 @@ namespace Services.RouteServices.Automation
 {
     public class InitialScheduler : ICellScheduler
     {
+        private bool initialScan;
+        private CellIndex initialCell;
+        private int lastNeighbour = -1;
+
         public IEnumerable<CellIndex> GetIndexRoute(IPose robotLocation, OccupancyGrid grid)
         {
-            throw new System.NotImplementedException();
+            yield return GetDestination(robotLocation, grid);
+        }
+        private CellIndex GetDestination(IPose robotLocation, OccupancyGrid grid)
+        {
+            if (initialScan)
+            {
+                initialScan = false;
+                initialCell = grid.GetIndex(robotLocation);
+                return initialCell;
+            }
+            else
+            {
+                lastNeighbour++;
+                switch (lastNeighbour)
+                {
+                    case 0:
+                        return new CellIndex(initialCell.X - 1, initialCell.Y);
+                    case 1:
+                        return new CellIndex(initialCell.X, initialCell.Y - 1);
+                    case 2:
+                        return new CellIndex(initialCell.X + 1, initialCell.Y);
+                    case 3:
+                        return new CellIndex(initialCell.X, initialCell.Y + 1);
+                    default:
+                        lastNeighbour = -1;
+                        return GetDestination(robotLocation, grid);
+                }
+            }
         }
     }
 }
