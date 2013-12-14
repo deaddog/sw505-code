6d75262fcd57a4b00edef8798f315288620ab54b
deaddog <mikkelslsl@gmail.com>
2013-12-14 17:13:19 +0100

    Added a ICellScheduler interface for declaring schedulers using only cell indices.

---
+++ root/ProductCode/Services/RouteServices/CellSchedulerExtension.cs
@@ -0,0 +1,17 @@
+﻿using CommonLib.DTOs;
+using CommonLib.Interfaces;
+using Data;
+using System;
+using System.Collections.Generic;
+
+namespace Services.RouteServices
+{
+    public static class CellSchedulerExtension
+    {
+        public static IEnumerable<ICoordinate> GetRoute(this ICellScheduler scheduler, IPose robotLocation, OccupancyGrid grid)
+        {
+            foreach (CellIndex index in scheduler.GetIndexRoute(robotLocation, grid))
+                yield return grid.GetCellCenter(index);
+        }
+    }
+}
---
+++ root/ProductCode/Services/RouteServices/ICellScheduler.cs
@@ -0,0 +1,21 @@
+﻿using CommonLib.DTOs;
+using CommonLib.Interfaces;
+using Data;
+using System.Collections.Generic;
+
+namespace Services.RouteServices
+{
+    /// <summary>
+    /// Defines a route service, capable of determining a route for a robot using a collection of cell indices.
+    /// </summary>
+    public interface ICellScheduler
+    {
+        /// <summary>
+        /// Gets the next route for a robot, such that the robot scans at the end of the route.
+        /// </summary>
+        /// <param name="robotLocation">The current robot location.</param>
+        /// <param name="grid">The grid in which the robot moves.</param>
+        /// <returns>A collection of <see cref="CellIndex"/>s through which the robot should move.</returns>
+        IEnumerable<CellIndex> GetIndexRoute(IPose robotLocation, OccupancyGrid grid);
+    }
+}
