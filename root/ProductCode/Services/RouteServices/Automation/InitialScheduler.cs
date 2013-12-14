2e84c0c5070413656d8971ef68e3f6c36d906b28
deaddog <mikkelslsl@gmail.com>
2013-12-14 16:23:49 +0100

    Added an additional scheduler for handling the initial state in the automated scheduler.

---
+++ root/ProductCode/Services/RouteServices/Automation/InitialScheduler.cs
@@ -0,0 +1,14 @@
+﻿using CommonLib.Interfaces;
+using Data;
+using System.Collections.Generic;
+
+namespace Services.RouteServices.Automation
+{
+    public class InitialScheduler : IScheduler
+    {
+        public IEnumerable<ICoordinate> GetRoute(IPose robotLocation, OccupancyGrid grid)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
