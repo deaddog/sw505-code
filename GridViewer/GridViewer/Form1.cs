a1543b50a499cc2aa7f66404ee36cfb39cc0f60b
deaddog <mikkelslsl@gmail.com>
2013-12-18 13:05:20 +0100

    Removed the grid field from form1.

--- GridViewer/GridViewer/Form1.cs
+++ GridViewer/GridViewer/Form1.cs
@@ -13,18 +13,16 @@ namespace GridViewer
 {
     public partial class Form1 : Form
     {
-        private OccupancyGrid grid;
         private Size diffSize;
 
         public Form1()
         {
             InitializeComponent();
-            grid = new OccupancyGrid(30, 23, 10, -150, -115);
 
             diffSize = new Size(this.Width - gridControl.Width, this.Height - gridControl.Height);
             gridControl.Resize += gridControl_Resize;
 
-            gridControl.Grid = grid;
+            gridControl.Grid = new OccupancyGrid(30, 23, 10, -150, -115);
             gridControl.Image = Properties.Resources.emptyGrid;
         }
         private void gridControl_Resize(object sender, EventArgs e)
