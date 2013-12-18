bd7d90f8dc48b1ad65f370d1a0d6c478e62df148
deaddog <mikkelslsl@gmail.com>
2013-12-18 14:48:45 +0100

    Updated the grid viewer gui and implemented drag'n 'drop effects.

--- GridViewer/GridViewer/Form1.Designer.cs
+++ GridViewer/GridViewer/Form1.Designer.cs
@@ -28,29 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.gridControl = new GridViewer.OccupancyGridControl();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(12, 383);
+            this.trackBar1.Maximum = -1;
+            this.trackBar1.Minimum = -1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(673, 32);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = -1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(691, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Points: {0}";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(691, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Update Perfect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // gridControl
             // 
+            this.gridControl.AllowDrop = true;
             this.gridControl.AreaHeight = 231F;
             this.gridControl.AreaWidth = 308F;
             this.gridControl.BackColor = System.Drawing.SystemColors.ControlDark;
             this.gridControl.Image = null;
             this.gridControl.Location = new System.Drawing.Point(12, 12);
             this.gridControl.Name = "gridControl";
-            this.gridControl.Size = new System.Drawing.Size(802, 403);
+            this.gridControl.Size = new System.Drawing.Size(673, 365);
             this.gridControl.TabIndex = 0;
             this.gridControl.Text = "occupancyGridControl1";
+            this.gridControl.DragDrop += new System.Windows.Forms.DragEventHandler(this.gridControl_DragDrop);
+            this.gridControl.DragEnter += new System.Windows.Forms.DragEventHandler(this.gridControl_DragEnter);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(826, 427);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.gridControl);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Evaluator form";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +103,9 @@
         #endregion
 
         private OccupancyGridControl gridControl;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- GridViewer/GridViewer/Form1.cs
+++ GridViewer/GridViewer/Form1.cs
@@ -8,12 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SystemInterface.GUI.Controls;
 
 namespace GridViewer
 {
     public partial class Form1 : Form
     {
         private Size diffSize;
+        private OccupancyGrid[] grids = new OccupancyGrid[0];
+        private OccupancyGrid defaultGrid;
 
         public Form1()
         {
@@ -22,7 +25,8 @@ namespace GridViewer
             diffSize = new Size(this.Width - gridControl.Width, this.Height - gridControl.Height);
             gridControl.Resize += gridControl_Resize;
 
-            gridControl.Grid = new OccupancyGrid(30, 23, 10, -150, -115);
+            defaultGrid = new OccupancyGrid(30, 23, 10, -150, -115);
+            gridControl.Grid = defaultGrid;
             gridControl.Image = Properties.Resources.emptyGrid;
         }
         private void gridControl_Resize(object sender, EventArgs e)
@@ -31,6 +35,43 @@ namespace GridViewer
                 return;
             else
                 this.Size = new Size(gridControl.Width + diffSize.Width, gridControl.Height + diffSize.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SetPerfectGridForm form = new SetPerfectGridForm())
+                form.ShowDialog();
+        }
+
+        private void gridControl_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length == 1 && files[0].EndsWith(".log"))
+                    e.Effect = DragDropEffects.Copy;
+                else
+                    e.Effect = DragDropEffects.None;
+            }
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void gridControl_DragDrop(object sender, DragEventArgs e)
+        {
+            string file = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            this.grids = GridLogger.LoadGrids(file);
+
+            this.trackBar1.Value = -1;
+            this.trackBar1.Maximum = grids.Length - 1;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (trackBar1.Value == -1)
+                gridControl.Grid = defaultGrid;
+            else
+                gridControl.Grid = grids[trackBar1.Value];
         }
     }
 }
