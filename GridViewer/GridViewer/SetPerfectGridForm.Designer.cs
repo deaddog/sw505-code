cedf21a486c44600370ee31240917e4cc830fdf9
deaddog <mikkelslsl@gmail.com>
2013-12-18 13:40:19 +0100

    Added a form for setting 'the perfect grid'.

---
+++ GridViewer/GridViewer/SetPerfectGridForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace GridViewer
+{
+    partial class SetPerfectGridForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl = new GridViewer.OccupancyGridControl();
+            this.SuspendLayout();
+            // 
+            // gridControl
+            // 
+            this.gridControl.AreaHeight = 231F;
+            this.gridControl.AreaWidth = 308F;
+            this.gridControl.Image = null;
+            this.gridControl.Location = new System.Drawing.Point(12, 12);
+            this.gridControl.Name = "gridControl";
+            this.gridControl.Size = new System.Drawing.Size(632, 375);
+            this.gridControl.TabIndex = 0;
+            this.gridControl.Text = "occupancyGridControl1";
+            this.gridControl.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gridControl_MouseClick);
+            // 
+            // SetPerfectGridForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 399);
+            this.Controls.Add(this.gridControl);
+            this.Name = "SetPerfectGridForm";
+            this.Text = "SetPerfectGridForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OccupancyGridControl gridControl;
+    }
+}
---
+++ GridViewer/GridViewer/SetPerfectGridForm.cs
@@ -0,0 +1,44 @@
+﻿using CommonLib.DTOs;
+using Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridViewer
+{
+    public partial class SetPerfectGridForm : Form
+    {
+        private StateGrid grid;
+        private Size diffSize;
+
+        public SetPerfectGridForm()
+        {
+            InitializeComponent();
+
+            diffSize = new Size(this.Width - gridControl.Width, this.Height - gridControl.Height);
+            gridControl.Resize += gridControl_Resize;
+
+            gridControl.Grid = new OccupancyGrid(30, 23, 10, -150, -115);
+            gridControl.Image = Properties.Resources.emptyGrid;
+
+            this.grid = new StateGrid(30, 23);
+        }
+        private void gridControl_Resize(object sender, EventArgs e)
+        {
+            if (DesignMode)
+                return;
+            else
+                this.Size = new Size(gridControl.Width + diffSize.Width, gridControl.Height + diffSize.Height);
+        }
+
+        private void gridControl_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+    }
+}
